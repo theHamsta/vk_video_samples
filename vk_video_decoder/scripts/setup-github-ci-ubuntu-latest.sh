8c4307c33ce3e5eaf0d2548b179c76ecd13e2183
Stephan Seitz <sseitz@nvidia.com>
2023-03-08 13:42:12 +0100

    use ninja by default

--- vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
+++ vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
@@ -3,4 +3,4 @@
 script_dir=$(dirname "$0")
 apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build
 cd "${script_dir}"/..
-./update_external_sources.sh
+CMAKE_GENERATOR=Ninja ./update_external_sources.sh
