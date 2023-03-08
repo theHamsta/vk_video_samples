8ff512dd46821b784fdac211c79d85e57f5df6ca
Stephan Seitz <sseitz@nvidia.com>
2023-03-08 14:23:26 +0100

    install vulkan

--- vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
+++ vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
@@ -3,6 +3,6 @@
 set -e
 
 script_dir=$(dirname "$0")
-sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev libwayland-dev libxrandr-dev libxcb-ewmh-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev libwayland-dev libxrandr-dev libxcb-ewmh-dev libvulkan1
 cd "${script_dir}"/..
 CMAKE_GENERATOR=Ninja ./update_external_sources.sh
