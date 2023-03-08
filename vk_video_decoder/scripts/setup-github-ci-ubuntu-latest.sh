86cb25d58a0b4bcd3258666417742e1777bad24f
Stephan Seitz <sseitz@nvidia.com>
2023-03-08 14:08:46 +0100

    fixup

--- vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
+++ vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
@@ -3,6 +3,6 @@
 set -e
 
 script_dir=$(dirname "$0")
-sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build
+sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev libwayland-dev libxrandr-dev libxcb-ewmh-dev
 cd "${script_dir}"/..
 CMAKE_GENERATOR=Ninja ./update_external_sources.sh
