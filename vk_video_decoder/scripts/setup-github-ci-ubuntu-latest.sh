7300b1ba29c6d3550ec2036278c0aa47fd3322db
Stephan Seitz <sseitz@nvidia.com>
2023-03-08 13:22:35 +0100

    fixup

--- vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
+++ vk_video_decoder/scripts/setup-github-ci-ubuntu-latest.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 script_dir=$(dirname "$0")
-apt-get install -y libavcodec-dev libavformat-dev libavutil-dev
+apt-get install -y libavcodec-dev libavformat-dev libavutil-dev ninja-build
 cd "${script_dir}"/..
 ./update_external_sources.sh
