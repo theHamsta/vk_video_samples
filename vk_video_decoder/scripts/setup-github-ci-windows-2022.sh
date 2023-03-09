f4fd4441cd3496d8b847ea1d3d31905cda3ce8ff
Stephan Seitz <sseitz@nvidia.com>
2023-03-09 11:28:51 +0100

    install ffmpeg windows

--- vk_video_decoder/scripts/setup-github-ci-windows-2022.sh
+++ vk_video_decoder/scripts/setup-github-ci-windows-2022.sh
@@ -7,4 +7,7 @@ cd ${script_dir}/..
 chmod +x ./update_external_sources.bat
 ./update_external_sources.bat
 
-
+cd ..
+curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl-shared.zip --output ffmpeg.zip
+unzip ffmpeg.zip
+mv ffmpeg*-shared ffmpeg
